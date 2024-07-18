2c62a4f864c53a80dc0f5e1e13f9e294ea591448
nathsagar96 <nathsagar96@gmail.com>
2024-07-18 22:55:45 +0530

    use angular 16 and generate services using openapi spec

--- literati-hub-ui/src/app/app.component.spec.ts
+++ literati-hub-ui/src/app/app.component.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    declarations: [AppComponent]
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'literati-hub-ui' title`, () => {
+  it(`should have as title 'literati-hub-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('literati-hub-ui');
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, literati-hub-ui');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('literati-hub-ui app is running!');
   });
 });
---
+++ literati-hub-ui/src/app/services/models/book-response.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export interface BookResponse {
+  archived?: boolean;
+  authorName?: string;
+  cover?: string;
+  id?: number;
+  isbn?: string;
+  owner?: string;
+  rating?: number;
+  shareable?: boolean;
+  synopsis?: string;
+  title?: string;
+}
