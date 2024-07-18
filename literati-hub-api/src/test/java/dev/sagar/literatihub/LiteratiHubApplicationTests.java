26efdd584f9788bb0b37772d66a8815e840a36fb
nathsagar96 <nathsagar96@gmail.com>
2024-07-18 23:15:55 +0530

    add testcontainers adn fix maven build for api

--- literati-hub-api/src/test/java/dev/sagar/literatihub/LiteratiHubApplicationTests.java
+++ literati-hub-api/src/test/java/dev/sagar/literatihub/LiteratiHubApplicationTests.java
@@ -1,13 +1,37 @@
 package dev.sagar.literatihub;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class LiteratiHubApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+  @LocalServerPort private Integer port;
 
+  @DynamicPropertySource
+  static void configureProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", postgres::getJdbcUrl);
+    registry.add("spring.datasource.username", postgres::getUsername);
+    registry.add("spring.datasource.password", postgres::getPassword);
+  }
+
+  @BeforeAll
+  static void beforeAll() {
+    postgres.start();
+  }
+
+  @AfterAll
+  static void afterAll() {
+    postgres.stop();
+  }
+
+  @Test
+  void contextLoads() {}
 }
